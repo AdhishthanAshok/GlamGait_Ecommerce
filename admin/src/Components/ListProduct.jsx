91bb2374b82912f0fdab11d947ad88fff1197d40
Adhishthan <adhishthanashok@gmail.com>
2024-07-17 12:43:28 +0530

    reverst the edits on last commit

--- admin/src/Components/ListProduct.jsx
+++ admin/src/Components/ListProduct.jsx
@@ -81,9 +81,7 @@ const ListProduct = () => {
             {allProducts.map((product, index) => (
               <tr key={index} className="text-center">
                 <td className="py-2 px-4 border-b">
-                  <Link
-                    to={`https://glamgait-shopping.vercel.app/product/${product.id}`}
-                  >
+                  <Link to={`/product/${product.id}`}>
                     <img
                       className="mx-auto h-12 w-12 object-cover"
                       src={product.image}
