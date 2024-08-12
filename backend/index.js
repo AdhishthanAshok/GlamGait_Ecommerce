c3af5297c2a7168ac83047bbdbf1658b69c6bbcd
Adhishthan <adhishthanashok@gmail.com>
2024-08-12 15:37:12 +0530

    Removed terminal outputs

--- backend/index.js
+++ backend/index.js
@@ -38,20 +38,20 @@ app.get("/", (req, res) => {
   res.send("Express App is Running");
 });
 
-// Image Stoage Engine
+// Image Storage Engine
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "product_images",
-    public_id: (req, file) => `${file.fieldname}_${Date.now()}`,
+    public_id: (req, file) => `${file.fieldname}_${Date.now()}`, // publicly visible name of the file is the public id
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }); // uploading file to cloudinary storage through multer , instead of storing it locally first , we directly upload the file.
 
 // Creating upload Endpoint for images
-// app.use("/images", express.static("upload/images"));
+// app.use("/images", express.static("upload/images")); // for local testing , when cloudinary is not attached , and not deploying on vercel.
 
 app.post("/upload", upload.single("product"), (req, res) => {
   try {
@@ -124,9 +124,7 @@ app.post("/addproduct", async (req, res) => {
     new_price: req.body.new_price,
     old_price: req.body.old_price,
   });
-  console.log(product);
   await product.save();
-  console.log("Saved");
   res.json({
     success: true,
     name: req.body.name,
@@ -137,7 +135,6 @@ app.post("/addproduct", async (req, res) => {
 
 app.post("/removeproduct", async (req, res) => {
   await Product.findOneAndDelete({ id: req.body.id });
-  console.log("Removed");
   res.json({
     success: true,
     name: req.body.name,
@@ -148,7 +145,6 @@ app.post("/removeproduct", async (req, res) => {
 
 app.get("/allproducts", async (req, res) => {
   let products = await Product.find({});
-  console.log("All Products Fetched");
   res.send(products);
 });
 
@@ -250,8 +246,6 @@ const fetchUser = async (req, res, next) => {
 
 // creating endpoint for newCollection Data
 app.post("/addtocart", fetchUser, async (req, res) => {
-  console.log("Added", req.body.itemId);
-  console.log(req.body, req.user);
   let userData = await Users.findOne({ _id: req.user.id });
   userData.cartData[req.body.itemId] += 1;
   await Users.findOneAndUpdate(
@@ -264,7 +258,6 @@ app.post("/addtocart", fetchUser, async (req, res) => {
 // creating endpoint to remove the product from cartData
 
 app.post("/removefromcart", fetchUser, async (req, res) => {
-  console.log("Removed", req.body.itemId);
   let userData = await Users.findOne({ _id: req.user.id });
   if (userData.cartData[req.body.itemId] > 0) {
     userData.cartData[req.body.itemId] -= 1;
@@ -277,7 +270,6 @@ app.post("/removefromcart", fetchUser, async (req, res) => {
 });
 
 app.post("/removeentireitem", fetchUser, async (req, res) => {
-  console.log("Removed Entire Item", req.body.itemId);
   let userData = await Users.findOne({ _id: req.user.id });
 
   userData.cartData[req.body.itemId] = 0;
@@ -292,7 +284,6 @@ app.post("/removeentireitem", fetchUser, async (req, res) => {
 // creating endpoint to get cart data of the user
 
 app.post("/getcart", fetchUser, async (req, res) => {
-  console.log("Get Cart");
   let userData = await Users.findOne({ _id: req.user.id });
   res.json(userData.cartData);
 });
